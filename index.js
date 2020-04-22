3d48f1a10d715e7cabd69f2a3e747eab9c75c168
Edson Bruno <edsonbruno415@gmail.com>
2020-04-22 20:57:29 -0300

    initial project

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send('Hello World!');
+});
+
+app.listen(port, (err, result) => console.log('Application on running on port '+port));
