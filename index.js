aaec47ac78aa5621ac40cf102ff7e19ba70cd84f
Edson Bruno <edsonbruno415@gmail.com>
2020-04-22 21:57:45 -0300

    Adding formatted pages website

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.send('Hello World!');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-app.listen(port, (err, result) => console.log('Application on running on port '+port));
+app.listen(port, (err, result) => console.log('Application on running on port ' + port));
