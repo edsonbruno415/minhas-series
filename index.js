a6ab699eec6864542bdc51f0fe546e458ffa34c8
Edson Bruno <edsonbruno415@gmail.com>
2020-04-22 22:07:20 -0300

    Adding routes from pages

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const app = express();
 
+const pages = require('./routes/pages');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.use('/', pages);
 
 app.listen(port, (err, result) => console.log('Application on running on port ' + port));
