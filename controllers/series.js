f5edca0344a61ebfd50042863d9eb8b6c92aa3f2
Edson Bruno <edsonbruno415@gmail.com>
2020-04-23 19:26:09 -0300

    Fixing template to update form

--- controllers/series.js
+++ controllers/series.js
@@ -36,9 +36,25 @@ const createNewForm = (req, res) => {
     res.render('form');
 }
 
+const createUpdateForm = (req, res) => {
+    const labels = {
+        'to-watch' : 'Para Assistir',
+        'watching' : 'Assistindo',
+        'watched' : 'Assistido'
+    }
+    Series.findById(req.params.id, (err, serie)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.render('form', { serie , labels });
+        }
+    });
+}
+
 module.exports = {
     index,
     deleteSerie,
     createNewForm,
-    createSerie
+    createSerie,
+    createUpdateForm
 }
--- routes/series.js
+++ routes/series.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const series = require('../controllers/series');
 
 router.get('/', series.index );
-router.get('/excluir/:id', series.deleteSerie );
+router.get('/excluir/:id', series.deleteSerie);
 router.get('/nova-serie', series.createNewForm);
 router.post('/novo-registro', series.createSerie);
+router.get('/editar/:id', series.createUpdateForm);
 
 module.exports = router;
