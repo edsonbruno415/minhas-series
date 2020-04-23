fbd7b161d975e9e2b2eadb6bb50c8806e98e4545
Edson Bruno <edsonbruno415@gmail.com>
2020-04-23 17:58:16 -0300

    new feature to list series

--- controllers/series.js
+++ controllers/series.js
@@ -1,6 +1,16 @@
-const index = (req, res) => res.render('series');
+const Series = require('../models/series');
+
+const index = (req, res) => {
+    Series.find({},(err, series )=>{
+        if(err){
+            console.error(err);
+        }else{
+            console.log(series);
+            res.render('series', { series });
+        }
+    });
+};
 
 module.exports = {
-    index,
-    
+    index
 }
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 const pages = require('./routes/pages');
+const mongoose = require('mongoose');
+const Serie = require('./models/series');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,4 +15,12 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use('/', pages);
 
-app.listen(port, (err, result) => console.log('Application on running on port ' + port));
+mongoose.connect('mongodb://localhost:27017/minhas-series',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            app.listen(port, (err, result) => console.log('Application on running on http://localhost:' + port));
+        }
+    });
